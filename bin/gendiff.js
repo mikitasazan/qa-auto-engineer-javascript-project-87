611ac5c5c89ed2eed1dc0ca48966b55126e2b04c
Mikita <mikit.sazan@gmail.com>
2026-09-01 23:19:44 +0300

    Step 3: accept filepath arguments and -f/--format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .helpOption('-h, --help', 'output usage information')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
